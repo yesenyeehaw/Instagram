59e0ad31726a1fa1878ad283c58dd00f4db52b27
yesenyeehaw <andrade.yesenia703@gmail.com>
2021-07-06 18:02:34 -0400

    Log Out implemented!

--- app/src/main/java/com/codepath/instagram/MainActivity.java
+++ app/src/main/java/com/codepath/instagram/MainActivity.java
@@ -1,13 +1,33 @@
 package com.codepath.instagram;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseUser;
+
 public class MainActivity extends AppCompatActivity {
+    Button btnLogOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogOut = findViewById(R.id.btnLogOut);
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                goLoginActivity(); //take us back to login activity
+            }
+        });
+    }
+    private void goLoginActivity(){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
     }
 }
